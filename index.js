4958cbc762e2ff19d5d586b61c65507fac66c9f9
Gabriel Oliveira <gabriel.oliveira@acovisa.local>
2026-07-02 14:38:54 -0300

    `Aula 17` criando API de contatos

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ const graphql = require("./src/graphql");
 
 const server = new ApolloServer({
   ...graphql,
-  formatError: (err) => {
-    if (err.message.startsWith("Usuario Existente:")) {
-      return new Error(err.message);
-    }
-
-    return err;
-  },
 });
 
 server.listen().then(({ url }) => console.log(url));
