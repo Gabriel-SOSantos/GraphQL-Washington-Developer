c96f1b86eb6fef37526efb48148996d3f3c5c8d9
Gabriel Oliveira <gabriel.oliveira@acovisa.local>
2026-07-03 10:08:56 -0300

    `Aula 21` Criando o CRUD Tasks e Usando Context

---
+++ src/graphql/modules/task/resolver.js
@@ -0,0 +1,28 @@
+module.exports = {
+  Query: {
+    async tasks(_, __, { dataSources, user_id }) {
+      return await dataSources.tasksRegisterService.getTasks(user_id);
+    },
+
+    async task(_, { id }, { dataSources, user_id }) {
+      return await dataSources.tasksRegisterService.showTaskById(user_id, id);
+    },
+  },
+
+  Mutation: {
+    async createTask(_, { data }, { dataSources, user_id }) {
+      return await dataSources.tasksRegisterService.addTask(user_id, data);
+    },
+
+    async deleteTask(_, { id }, { dataSources, user_id }) {
+      return await dataSources.tasksRegisterService.deleteTask(user_id, id);
+    },
+    async updateTask(_, { id, data }, { dataSources, user_id }) {
+      return await dataSources.tasksRegisterService.updateTask(
+        user_id,
+        id,
+        data,
+      );
+    },
+  },
+};
